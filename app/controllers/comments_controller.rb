3869f25dd7418c0d0b523a823ae103065676a737
hakozaru <t.morii@everyleaf.com>
2016-02-28 17:57:10 +0900

    Fix comment CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
     @comment = @post.comment.create(comment_params)
-    redirect_to post_path(@post)
+    redirect_to user_post_path(@user, @post)
   end
 
   private
