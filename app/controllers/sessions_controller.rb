22de9b566d3f5a2899689fc24b11f650e3ff811e
hakozaru <t.morii@everyleaf.com>
2016-02-28 15:26:37 +0900

    Add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user
+      log_in user
+      redirect_to user
+    else
+      flash.now[:invalid] = 'ログインに失敗しました'
+      render 'new'
+    end
   end
 
   def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  # 渡されたユーザーでログイン
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
