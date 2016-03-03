6fe9857049e94b7ebb93a06afd098303274996a4
hakozaru <t.morii@everyleaf.com>
2016-03-03 21:27:51 +0900

    DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,13 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, only: [:index, :show, :new, :edit, :create]
   before_action :user_check
 
   def index
-    @user = User.find_by(id: session[:user_id])
     @posts = Post.where(user_id: session[:user_id]).all
   end
 
   def show
-    @user = User.find_by(id: session[:user_id])
     if @post == nil
       flash[:could_not_find_post] = "記事が見つかりません"
       redirect_to user_posts_path
@@ -16,16 +15,13 @@ class PostsController < ApplicationController
   end
 
   def new
-    @user = User.find_by(id: session[:user_id])
     @post = Post.new
   end
 
   def edit
-    @user = User.find_by(id: session[:user_id])
   end
 
   def create
-    @user = User.find_by(id: session[:user_id])
     @post = @user.post.new(post_params)
     if @post.save
       redirect_to user_posts_path
@@ -54,5 +50,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:name, :title, :tfield)
+  end
+
+  def find_user
+    @user = User.find_by(id: session[:user_id])
   end
 end
