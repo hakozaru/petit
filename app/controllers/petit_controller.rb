9834ecb3e818cf1b7d417ff4937bcfbdd81df276
hakozaru <t.morii@everyleaf.com>
2016-04-09 16:37:50 +0900

    Fix comment

--- app/controllers/petit_controller.rb
+++ app/controllers/petit_controller.rb
@@ -9,7 +9,7 @@ class PetitController < ApplicationController
       # ユーザー登録用
       @user = User.new
 
-      # トップページだけbodyにクラス追加して、背景を表示する
+      # bodyにクラス追加して、背景を表示する
       @body = "petit_index"
       @footer = "footer_index"
     end
